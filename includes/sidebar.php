251148af2f0da1d37b95a27197e87e49c378192a
Kawser <kawserahmedpk2017@gmail.com>
2024-09-05 16:40:29 +0600

    first commit

---
+++ includes/sidebar.php
@@ -0,0 +1,25 @@
+<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+		<div class="profile-sidebar">
+		<div class="profile-userpic">
+				<img src="https://www.w3schools.com/howto/img_avatar.png" width="20" class="img-responsive" alt="Icon">
+			</div>
+			<div class="profile-usertitle">
+				<div class="profile-usertitle-name">Admin</div>
+				<div class="profile-usertitle-status"><span class="indicator label-success"></span>Online</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="divider"></div>
+
+		
+		<ul class="nav menu">
+			<li class="<?php if($page=="dashboard") {echo "active";}?>"><a href="dashboard.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+			<li class="<?php if($page=="vehicle-category") {echo "active";}?>"><a href="vehicle-category.php"><em class="fa fa-th-large">&nbsp;</em> Vehicle Category</a></li>
+			<li class="<?php if($page=="manage-vehicles") {echo "active";}?>"><a href="manage-vehicles.php"><em class="fa fa-car">&nbsp;</em> Vehicle Entry</a></li>
+			<li class="<?php if($page=="in-vehicle") {echo "active";}?>"><a href="#"><em class="fa fa-toggle-on">&nbsp;</em> IN Vehicles</a></li>
+            <li class="<?php if($page=="out-vehicle") {echo "active";}?>"><a href="#"><em class="fa fa-toggle-off">&nbsp;</em> OUT Vehicles</a></li>
+			
+			
+		</ul>
+		
+	</div><!--/.sidebar-->
